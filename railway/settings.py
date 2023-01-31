10e871df9bbccf1a5bc6c305cc3949ebc6950e76
anil.bhutale <anil.bhutale@hedgehoglab.com>
2023-01-31 23:22:05 +0530

    test

--- railway/settings.py
+++ railway/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'railway.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'nZY5Kw3esooWVGa2OJNZ',
+       'HOST': 'containers-us-west-123.railway.app',
+       'PORT': '6526',
+   }
 }
+
 
 
 # Password validation
